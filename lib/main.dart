2ca3e2ce161e150e07d7b26afb3aae3ba7793946
LUSEDOU <lusedou@gmail.com>
2023-12-10 09:47:58 -0500

    fix: runeZoneGuarded

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:u_search_flutter/bootstrap.dart';
 import 'package:u_search_flutter/clients.dart';
 import 'package:u_search_flutter/observer.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
   Bloc.observer = AppBlocObserver();
 
   final authClient = AuthClientTest();
