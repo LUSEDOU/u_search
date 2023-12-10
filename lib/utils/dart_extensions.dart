5d70083b1f54ee751a46c2d246c6ce98a7a46cdd
LUSEDOU <lusedou@gmail.com>
2023-12-10 09:48:41 -0500

    enhancement(utils): add some extensions

--- lib/utils/dart_extensions.dart
+++ lib/utils/dart_extensions.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 extension IterableExtension<T> on Iterable<T> {
   Iterable<T?> whereNotNull() => where((T? element) => element != null);
 
@@ -28,4 +30,12 @@ extension IterableExtension<T> on Iterable<T> {
       return e;
     });
   }
+}
+
+extension ContextExtension on BuildContext {
+    Size get size => MediaQuery.of(this).size;
+    double get width => size.width;
+    double get height => size.height;
+
+    ThemeData get theme => Theme.of(this);
 }
