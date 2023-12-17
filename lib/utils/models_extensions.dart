3389c3002b25d102cb6b87a1593c9d58a76779c1
LUSEDOU <dolorier_uribe@hotmail.com>
2023-12-17 13:16:08 -0500

    fix

--- lib/utils/models_extensions.dart
+++ lib/utils/models_extensions.dart
@@ -1,13 +1,13 @@
-import 'package:authentication_repository/authentication_repository.dart' as auth;
+import 'package:authentication_repository/authentication_repository.dart'
+    as auth;
 import 'package:data_repository/data_repository.dart';
 
 extension UserX on auth.User {
-  User toModel() {
+  User toModel({String? name}) {
     return User(
       email: email ?? '',
-      name: name ?? '',
+      name: name ?? this.name ?? '',
       id: id,
     );
   }
 }
-
