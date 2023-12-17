0af6d18a6836ecf05310a0def86eb1260c118506
LUSEDOU <dolorier_uribe@hotmail.com>
2023-12-17 09:38:02 -0500

    refactor(contest)

--- packages/data_repository/lib/src/models/contest.dart
+++ packages/data_repository/lib/src/models/contest.dart
@@ -1,30 +1,42 @@
 import 'package:data_repository/data_repository.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-part 'contest.freezed.dart';
 part 'contest.g.dart';
 
 /// {@template contest}
-/// A model representing a contest for an financial aid.
+/// A model representing a contest for financial aid.
 /// {@endtemplate}
-@freezed
-class Contest with _$Contest {
-  /// {@macro contest}
-  const factory Contest({
-    /// The id of the contest
-    @JsonKey(required: true) required int id,
-
-    /// The name of the contest
-    @JsonKey(required: true) required String name,
-
-    /// The description of the contest
-    @JsonKey(required: true) required String description,
-
-    /// The criterias of the contest
-    @JsonKey(required: true) required List<Criteria> criterias,
-  }) = _Contest;
+@JsonSerializable()
+class Contest {
+  /// Creates an instance of [Contest].
+  const Contest({
+    required this.id,
+    this.name = '',
+    this.description = '',
+    this.criterias = const [],
+  });
 
   /// Converts a Contest from a json map
   factory Contest.fromJson(Map<String, dynamic> json) =>
       _$ContestFromJson(json);
+
+  /// The id of the contest
+  @JsonKey(required: true)
+  final int id;
+
+  /// The name of the contest
+  @JsonKey(required: true)
+  final String name;
+
+  /// The description of the contest
+  @JsonKey(required: true)
+  final String description;
+
+  /// The criterias of the contest
+  @JsonKey(required: true)
+  final List<Criteria> criterias;
+
+  /// Converts a [Contest] instance into a [Map<String, dynamic>].
+  Map<String, dynamic> toJson() => _$ContestToJson(this);
 }
+
