12ec23010f5c41607b5091a2e97bf41102ba9fc8
LUSEDOU <dolorier_uribe@hotmail.com>
2023-12-17 14:38:50 -0500

    refactor

--- lib/utils/models_extensions.dart
+++ lib/utils/models_extensions.dart
@@ -11,3 +11,46 @@ extension UserX on auth.User {
     );
   }
 }
+
+extension RoleTypeX on RoleType {
+  /// Role is researcher
+  bool get isResearcher => this == RoleType.researcher;
+
+  /// Role is reviewer
+  bool get isReviewer => this == RoleType.reviewer;
+
+  /// Role is admin
+  bool get isAdmin => this == RoleType.admin;
+
+  /// Role is unknown
+  bool get isUnknown => this == RoleType.unknown;
+
+  /// Role is permitted
+  bool get isPermitted => RoleType.permitted.contains(this);
+
+  /// Role is not permitted
+  bool get isNotPermitted => !isPermitted;
+}
+
+extension RoleX on Role {
+  /// Role is researcher
+  bool get isResearcher => type.isResearcher;
+
+  /// Role is reviewer
+  bool get isReviewer => type.isReviewer;
+
+  /// Role is admin
+  bool get isAdmin => type.isAdmin;
+
+  /// Role is unknown
+  bool get isUnknown => type.isUnknown;
+
+  /// Role is permitted
+  bool get isPermitted => type.isPermitted;
+
+  /// Role is not permitted
+  bool get isNotPermitted => type.isNotPermitted;
+
+  /// Role is created
+  bool get isCreated => id != -1;
+}
