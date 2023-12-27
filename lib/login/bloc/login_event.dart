71bac6b589a17b33ea3c2a13e26291ea069d926f
LUSEDOU <dolorier_uribe@hotmail.com>
2023-12-27 14:51:55 -0500

    feat(login): create bloc

---
+++ lib/login/bloc/login_event.dart
@@ -0,0 +1,19 @@
+part of 'login_bloc.dart';
+
+sealed class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoginEmailChanged extends LoginEvent {
+  const LoginEmailChanged(this.email);
+
+  final String email;
+
+  @override
+  List<Object> get props => [email];
+}
+
+class SendEmailLinkSubmitted extends LoginEvent {}
