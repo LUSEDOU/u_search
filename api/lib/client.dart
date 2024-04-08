61c47998b994b6c170b75adfa582dc24acfb099d
LUSEDOU <dolorier_uribe@hotmail.com>
2024-04-07 22:08:03 -0500

    client

--- api/lib/client.dart
+++ api/lib/client.dart
@@ -1,1 +1,5 @@
-export 'src/client/u_search_api_client.dart';
+export 'src/client/u_search_api_client.dart' show USearchApiClient;
+
+export 'src/data/models/models.dart' show User;
+
+export 'src/models/models.dart' show LoginResponse, SubscribeResponse;
